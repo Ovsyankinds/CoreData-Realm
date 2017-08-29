7e4d99b95cbf0b81b772840d719d58e5a86af2c8
Ovsyankinds <ovsyankinds@Ovsyankindss-iMac.local>
2017-08-29 17:38:24 +0400

    1 commit

--- CoreData&Realm/ViewController.swift
+++ CoreData&Realm/ViewController.swift
@@ -7,12 +7,70 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var inputName: UITextField?
+    @IBOutlet weak var inputSername: UITextField?
+    @IBOutlet weak var inputAge: UITextField?
+    
+    @IBOutlet weak var name: UILabel?
+    @IBOutlet weak var sername: UILabel?
+    @IBOutlet weak var age: UILabel?
+    
+    var array = [Person]()
+    
+    var object: NSManagedObjectContext!
+    
+    @IBAction func submitToDatabase(){
+        let dbItem = Person(context: object)
+        dbItem.name = inputName?.text!
+        dbItem.sername = inputSername?.text!
+        dbItem.age = Int16(inputAge!.text!)!
+        
+        do{
+            try self.object.save()
+        } catch{
+            print("Not load data")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let person = array[indexPath.row]
+        cell.nameLabel!.text = person.name
+        cell.sernameLabel!.text = person.sername
+        cell.ageLabel!.text = person.sername
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        object = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let request: NSFetchRequest<Person> = Person.fetchRequest()
+        tableView.reloadData()
+        
+        do{
+            let array = try object.fetch(request)
+            
+            /*name?.text = array[0]
+            sername?.text = array[1]
+            age?.text = String(describing: array[2])*/
+            
+            /*for row in result{
+                print(row.name! , row.sername!, row.age)
+            }*/
+            
+        } catch{
+            print("Not reques data")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
